f79a527d8476dbe6976bba39e44b1f076efb223b
Alan McConchie <alan.mcconchie@gmail.com>
2013-06-19 20:07:59 -0700

    Updated URLs (changed - to _)

--- fetchall-vector.sh
+++ fetchall-vector.sh
@@ -6,12 +6,12 @@
 # Packages are also available in a single bundle here:
 # http://www.nacis.org/naturalearth/packages/natural_earth_vector.zip
 
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m-cultural.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/10m-physical.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/10m_cultural.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/physical/10m_physical.zip
 
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/50m-cultural.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/physical/50m-physical.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/50m_cultural.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/physical/50m_physical.zip
 
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/110m-cultural.zip
-wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/physical/110m-physical.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/110m_cultural.zip
+wget http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/physical/110m_physical.zip
 
